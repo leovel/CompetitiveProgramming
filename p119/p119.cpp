206b0aa0b7554ec129dd67d704f55152f3cd38fe
Leovel Camilo Neto Arias <leovel@gmail.com>
2019-08-21 16:28:49 +0100

    Initial commit

---
+++ p119/p119.cpp
@@ -0,0 +1,18 @@
+// p119.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main()
+{
+	const char* msg = "Hello World!\n";
+	printf("%s ", msg);
+}
