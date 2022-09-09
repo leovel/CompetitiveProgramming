bf06817686d5f6a7c2da1875a77ba82a301d3670
Leovel Camilo Neto <leovel@gmail.com>
2022-09-09 09:42:18 +0100

    usando long long

--- p10339/p10339.cpp
+++ p10339/p10339.cpp
@@ -2,16 +2,18 @@
 #include <cmath>
 #include <cstdio>
 
+typedef long long ll;
+
 int main()
 {
-	unsigned int k, m, segundos, horas, minutos;
-	while (scanf("%u %u", &k, &m) != EOF)
+	ll k, m, segundos, horas, minutos;
+	while (scanf("%lld %lld", &k, &m) != EOF)
 	{
 
-		segundos =  (43200 * (86400 - k) / abs((int)(k - m))) % 43200 + 30;
+		segundos =  (43200 * (86400 - k) / abs(k - m)) % 43200 + 30;
 		horas = (segundos / 3600) % 12;
 		minutos = (segundos % 3600) / 60;
 
-		printf("%u %u %02u:%02u\n", k, m, horas == 0 ? 12 : horas, minutos);
+		printf("%lld %lld %02lld:%02lld\n", k, m, horas == 0 ? 12 : horas, minutos);
 	}
 }
