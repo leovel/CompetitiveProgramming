afad4b3e9d33c9aedac7652bb57b53fba56a3e16
Leovel Camilo Neto <leovel@gmail.com>
2024-05-16 22:56:00 +0100

    baloni

---
+++ baloni/baloni.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int popped[1000002], N, Hi, shoots;
+
+int main()
+{
+    while (scanf("%d", &N) != EOF) {
+        shoots = 0;
+        memset(&popped, 0, sizeof(popped));
+        for (int i = 0; i < N; i++)
+        {
+            scanf("%d", &Hi);
+
+            popped[Hi]++;
+
+            if (popped[Hi + 1] < 1)
+            {
+                shoots++;
+            }
+            else
+                popped[Hi + 1]--;
+        }
+        printf("%d\n", shoots);
+    }
+}
