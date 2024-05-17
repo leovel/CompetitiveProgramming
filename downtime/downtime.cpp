6d6311501fb61cb66fc0aac345c9701078165086
Leovel Camilo Neto <leovel@gmail.com>
2024-05-17 17:11:40 +0100

    downtime [Solved]

--- downtime/downtime.cpp
+++ downtime/downtime.cpp
@@ -6,32 +6,27 @@
 
 using namespace std;
 
-int request[100002], n, k, ti, lindex = 0, max_requests = 0, current = 0;
+int request[100002], n, k, lindex = 0, max_requests = 0, current = 0;
 
 int main()
 {
     while (scanf("%d %d", &n, &k) != EOF) {
         lindex = 0; max_requests = 0; current = 0;
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%d", &request[i]);     
-        }
 
         for (int i = 0; i < n; i++)
         {
-            ti = request[i];
-            if (ti < request[lindex] + 1000)
-                current++;
-            else
+            scanf("%d", &request[i]);
+
+            max_requests = max(max_requests, current);
+
+            while (request[i] >= request[lindex] + 1000)
             {
-                max_requests = max(max_requests, current);
+                lindex++;
+                if (current > 0) current--;
+            };
 
-                if (current > 1)
-                    current--;
-
-                if (lindex < i)
-                    lindex++;
-            }
+            if (request[i] < request[lindex] + 1000)
+                current++;
         }
         max_requests = max(max_requests, current);
 
