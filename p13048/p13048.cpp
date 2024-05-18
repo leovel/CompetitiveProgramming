ebdc76fd634d36a1b9c8114260173fe561a5bc2d
Leovel Camilo Neto <leovel@gmail.com>
2024-05-18 18:03:58 +0100

    p13048

---
+++ p13048/p13048.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int T, l, Y;
+char S[52];
+
+int main()
+{
+    scanf("%d", &T);
+
+    for (int X = 1; X <= T; X++)
+    {
+        scanf("%s", &S);
+        l = strlen(S); Y = 0;
+
+        for (int i = 0; i < l; i++)
+        {
+            if(S[i] == '-' &&
+                (i - 1 < 0 || S[i - 1] != 'S') &&
+                (i + 1 >= l || (S[i + 1] != 'S' && S[i + 1] != 'B')) &&
+                (i + 2 >= l || (S[i + 2] != 'B')))
+            {
+                Y++;
+            }
+        }
+
+        printf("Case %d: %d\n", X, Y);
+    }
+}
