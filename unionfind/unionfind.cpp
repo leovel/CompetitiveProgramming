1d6254c8d8dc61d4ff55dda350f23601b8629966
Leovel Camilo Neto <leovel@gmail.com>
2024-05-20 21:14:02 +0100

    unionfind and p1197

---
+++ unionfind/unionfind.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+class UnionFind {                                // OOP style
+private:
+    vi p, rank, setSize;                           // vi p is the key part
+    int numSets;
+public:
+    UnionFind(int N) {
+        p.assign(N, 0); for (int i = 0; i < N; ++i) p[i] = i;
+        rank.assign(N, 0);                           // optional speedup
+        setSize.assign(N, 1);                        // optional feature
+        numSets = N;                                 // optional feature
+    }
+
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+
+    int numDisjointSets() { return numSets; }      // optional
+    int sizeOfSet(int i) { return setSize[findSet(i)]; } // optional
+
+    void unionSet(int i, int j) {
+        if (isSameSet(i, j)) return;                 // i and j are in same set
+        int x = findSet(i), y = findSet(j);          // find both rep items
+        if (rank[x] > rank[y]) swap(x, y);           // keep x 'shorter' than y
+        p[x] = y;                                    // set x under y
+        if (rank[x] == rank[y]) ++rank[y];           // optional speedup
+        setSize[y] += setSize[x];                    // combine set sizes at y
+        --numSets;                                   // a union reduces numSets
+    }
+};
+
+int N, Q, a, b;
+char op[2];
+
+int main()
+{
+    while (scanf("%d %d", &N, &Q) != EOF)
+    {
+        UnionFind UF(N);
+        while (Q--)
+        {
+            scanf("%s %d %d", &op, &a, &b);
+            if (op[0] == '=')
+                UF.unionSet(a, b);
+            else
+                printf(UF.isSameSet(a, b) ? "yes\n" : "no\n");
+        }
+    }
+}
+
