10c48711a8c8124b42150ae4d5c50c699b784bdc
Leovel Camilo Neto <leovel@gmail.com>
2024-05-24 18:37:59 +0100

    epigdanceoff

---
+++ epigdanceoff/epigdanceoff.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+#include <bitset>
+
+using namespace std;
+
+int N, M, qtd;
+bitset<2001> mask;
+char line[2001];
+
+int main()
+{
+    while (scanf("%d %d", &N, &M) != EOF)
+    {
+        mask.reset();
+        qtd = 1;
+
+        while(N--)
+        {
+            scanf("%s", &line);
+
+            for (size_t i = 0; i < M; i++)
+            {
+                if (line[i] == '$')
+                {
+                    mask.set(i);
+                }
+            }
+        }
+
+        printf("%d\n", M - mask.count() + 1);
+    }
+}
