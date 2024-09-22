90b4584571a030b9dc362000f73c65fb27f0e5f2
Leovel Camilo Neto <leovel@gmail.com>
2024-09-22 13:24:07 +0100

    p11462

---
+++ p11462/p11462.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ages[102], age, age_count;
+
+int main()
+{
+    while (scanf("%d", &n) && n)
+    {
+        memset(&ages, 0, sizeof ages);
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &age);
+            ages[age]++;
+        }
+
+        age_count = 0;
+        for (int i = 1; i < 101; i++)
+        {
+            if(ages[i])
+            {
+                for (int a = 0; a < ages[i]; a++)
+                {
+                    age_count == 0 ? printf("%d", i) : printf(" %d", i);
+                    age_count++;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
