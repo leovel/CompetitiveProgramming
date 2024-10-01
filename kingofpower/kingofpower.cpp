6f0764b84ed3596f102eccb711f29685dcb0e032
Leovel Camilo Neto <leovel@gmail.com>
2024-10-01 17:43:10 +0100

    p11933

--- kingofpower/kingofpower.cpp
+++ kingofpower/kingofpower.cpp
@@ -6,24 +6,55 @@ long long m, k, p = 1000000007;
 
 long long S;
 
-long long power(long long x, long long y, long long p)
-{
-    long long res = 1;     // Initialize result 
+//long long power(long long x, long long y, long long p)
+//{
+//    long long res = 1;     // Initialize result 
+//
+//    x = x % p; // Update x if it is more than or 
+//    // equal to p
+//
+//    if (x == 0) return 0; // In case x is divisible by p;
+//
+//    while (y > 0)
+//    {
+//        // If y is odd, multiply x with result 
+//        if (y & 1)
+//            res = (res * x) % p;
+//
+//        // y must be even now 
+//        y = y >> 1; // y = y/2 
+//        x = (x * x) % p;
+//    }
+//    return res;
+//}
 
-    x = x % p; // Update x if it is more than or 
-    // equal to p
+typedef long long ll;
+ll binary_exponent(ll x, ll y, ll mod) {
+    ll res = 1;
+    ll p = x % mod;
+    while (y) {
+        if (y & 1) {
+            res = (res * p) % mod;
+        }
+        p = (p * p) % mod;
+        y = y >> 1;
+    }
+    return res;
+}
 
-    if (x == 0) return 0; // In case x is divisible by p;
-
-    while (y > 0)
-    {
-        // If y is odd, multiply x with result 
-        if (y & 1)
-            res = (res * x) % p;
-
-        // y must be even now 
-        y = y >> 1; // y = y/2 
-        x = (x * x) % p;
+ll gp_sum(ll a, int n, ll mod) {
+    ll A = 1;
+    int num = 0;
+    ll res = 0;
+    ll degree = 1;
+    while (n) {
+        if (n & (1 << num)) {
+            n &= (~(1 << num));
+            res = (res + (A * binary_exponent(a, n, mod)) % mod) % mod;
+        }
+        A = (A + (A * binary_exponent(a, degree, mod)) % mod) % mod;
+        degree *= 2;
+        num++;
     }
     return res;
 }
@@ -31,15 +62,9 @@ long long power(long long x, long long y, long long p)
 int main()
 {
     S = 0;
-    m = 3;
-    k = 1000;
+    m = 97;
+    k = 100000;
 
-    for (int i = 1; i <= k; i++)
-    {
-        S += power(m, i, p) * power(i, m, p);
-        S = S % p;
-
-        printf("%lld\n", S);
-    }
+    printf("%lld\n", gp_sum(m, 1000000000, 1000000007));
 }
 
