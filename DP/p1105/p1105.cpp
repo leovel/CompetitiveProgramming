fad73a9981606319b7a9e5e453401a503db70883
Leovel Camilo Neto <leovel@gmail.com>
2025-07-28 14:32:33 +0100

    p1105

--- DP/p1105/p1105.cpp
+++ DP/p1105/p1105.cpp
@@ -3,58 +3,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx, dy, n, q, city_grid[1001][1001], xi, yi, m;
+int dx, dy, n, q, rgrid[2024][2024], xi, yi, rx, ry, m;
+
+void rotate(int x, int y, int dx, int dy, int& rx, int& ry) {
+    rx = x - y + dy, ry = x + y;
+}
 
 int main()
 {
     int tcase = 0;
     while (scanf("%d %d %d %d", &dx, &dy, &n, &q), dx || dy || n || q)
     {
-        memset(city_grid, 0, sizeof city_grid);
+
+        memset(rgrid, 0, sizeof rgrid);
         for (int i = 0; i < n; ++i)
         {
             scanf("%d %d", &xi, &yi);
-            city_grid[xi - 1][yi - 1] = 1;
+            rotate(xi, yi, dx, dy, rx, ry);
+            rgrid[rx][ry] = 1;
         }
 
-        for (int i = 0; i < dx; ++i)
-            for (int j = 0; j < dy; ++j) {
-                if (i > 0) city_grid[i][j] += city_grid[i - 1][j];
-                if (j > 0) city_grid[i][j] += city_grid[i][j - 1];
-                if (i > 0 && j > 0) city_grid[i][j] -= city_grid[i - 1][j - 1];
+        for (int i = 1; i <= dx + dy; ++i)
+            for (int j = 1; j <= dx + dy; ++j) {
+                rgrid[i][j] += rgrid[i - 1][j];
+                rgrid[i][j] += rgrid[i][j - 1];
+                rgrid[i][j] -= rgrid[i - 1][j - 1];
             }
 
         printf("Case %d:\n", ++tcase);
         for (int i = 0; i < q; ++i)
         {
             scanf("%d", &m);
-            m = 2 * m + 1;
-            //m = min(m, dx + dy);
-            int maxSubRect = -1;
 
-            int x = 0, y = 0;
+            int maxSubRect = 0;
 
-            for (int l = 0; l < dx; ++l)
-                for (int r = l; r < min(l + m + 1, dy); ++r) {
-                    int w = r - l;
-                    for (int row = 0; row < dx; ++row) {
-                        int a = row, b = l, c = min(dx - 1, row + m - w), d = r;
+            int x = 1, y = 1;
+            for (int j = 1; j <= dy; ++j) // priority to y-axis
+                for (int i = 1; i <= dx; ++i)
+                {
+                    rotate(i, j, dx, dy, rx, ry);
 
-                        int subRect = city_grid[c][d];
-                        if (a > 0) subRect -= city_grid[a - 1][d];
-                        if (b > 0) subRect -= city_grid[c][b - 1];
-                        if (a > 0 && b > 0) subRect += city_grid[a - 1][a - 1];
+                    int a = max(rx - m, 1); int b = max(ry - m, 1);
+                    int c = min(rx + m, dx + dy); int d = min(ry + m, dx + dy);
 
-                        if (subRect > maxSubRect)
-                        {
-                            maxSubRect = subRect;
-                            x = max(a, c - m/2); y = max(b, d - m/2);
-                        }
+                    int subRect = rgrid[c][d];
+                    subRect -= rgrid[a - 1][d];
+                    subRect -= rgrid[c][b - 1];
+                    subRect += rgrid[a - 1][b - 1];
+
+                    if (subRect > maxSubRect)
+                    {
+                        maxSubRect = subRect;
+                        x = i; y = j;
                     }
+
                 }
 
-            printf("%d (%d,%d)\n",maxSubRect, x + 1, y + 1);
+            printf("%d (%d,%d)\n", maxSubRect, x, y);
         }
-                
+
     }
 }
